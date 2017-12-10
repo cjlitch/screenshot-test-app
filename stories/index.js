6530a6516b6247f664f37bc01f3b7c2cd0f67eda
cjlitch <cjlitch@gmail.com>
2017-12-10 09:54:31 -0500

    tes: Update snapshot tests

--- stories/index.js
+++ stories/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
 import App from '../src/App';
-
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Goodbye Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+import BirthdayForm from '../src/components/BirthdayForm/BirthdayForm';
+import BirthdaySelect from '../src/components/BirthdaySelect/BirthdaySelect';
 
 storiesOf('App', module)
-  .add('with image', () => <App shouldHaveImage />)
-  .add('without image', () => <App />);
+  .add('default', () => <App />);
+
+storiesOf('BirthdayForm', module)
+  .add('default', () => <BirthdayForm />);
+
+storiesOf('BirthdaySelect', module)
+  .add('Month selection', () => <BirthdaySelect
+    label="Month"
+    options={[
+      { value: 1, label: "January" },
+      { value: 2, label: "February" },
+      { value: 3, label: "March" },
+    ]}
+    onChange={() => {}} />);
