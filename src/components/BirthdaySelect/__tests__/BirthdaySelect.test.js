872da8df4232486e65370b3010d4691ae0ab23d0
cjlitch <cjlitch@gmail.com>
2018-01-24 06:11:48 -0500

    test: Add snapshot tests

--- src/components/BirthdaySelect/__tests__/BirthdaySelect.test.js
+++ src/components/BirthdaySelect/__tests__/BirthdaySelect.test.js
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import BirthdaySelect from '../BirthdaySelect';
 import Select from 'react-select';
+import renderer from 'react-test-renderer';
+
+const defaultProps = {
+    options: [1, 2, 3],
+    label: "test",
+    onChange: () => {}
+};
 
 describe('BirthdaySelect', () => {
     it('has a select', () => {
-        const wrapper = shallow(
-            <BirthdaySelect
-                options={[1, 2, 3]}
-                label="test"
-                onChange={() => {}} />
-            );
-            
+        const wrapper = shallow(<BirthdaySelect {...defaultProps} />);
         expect(wrapper.find(Select)).toHaveLength(1);
+    });
+
+    it('matches snapshot', () => {
+        const wrapper = renderer
+            .create(<BirthdaySelect {...defaultProps} />)
+            .toJSON();
+
+        expect(wrapper).toMatchSnapshot();
     });
 });
 
